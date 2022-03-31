a163701e3703b1cc5a155776ec32cd56535a1783
xwwwb <xuwenbo@88.com>
2022-03-31 18:46:30 +0800

    使滚动变成异步任务

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -10,15 +10,16 @@ export default class Main extends Component {
 	myRef = React.createRef()
 
 	componentDidMount() {
-		console.log("这里执行Main的componentDidMount")
-		// 这里的宽度显示980 很奇怪
-		// 加载网页时执行滚动到当前周
-		this.myRef.current.scrollLeft = (this.state.nowWeek - 1) * (document.body.clientWidth)
-		// 这行是向nav组件传递当前周
-		this.props.handleWeekChanges(this.state.nowWeek)
+
 		setTimeout(() => {
 			console.log("这里是定时器 定时器放在生命周期函数中")
-		}, 10000)
+			console.log("这里执行Main的componentDidMount")
+			// 这里的宽度显示980 很奇怪
+			// 加载网页时执行滚动到当前周
+			this.myRef.current.scrollLeft = (this.state.nowWeek - 1) * (document.body.clientWidth)
+			// 这行是向nav组件传递当前周
+			this.props.handleWeekChanges(this.state.nowWeek)
+		})
 
 	}
 	handleScroll = (e) => {
