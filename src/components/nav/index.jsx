9ba157b8b1418c3af9b0842481986c3b89e42d1c
xwwwb <xuwenbo@88.com>
2022-03-31 15:38:55 +0800

    引入antd

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import './index.css'
-export default class Nav extends Component {
 
+export default class Nav extends Component {
+  handleClick = () => {
+
+  }
   render() {
     return (
-      <div id="nav">第{this.props.weekCount}周</div>
+      <>
+        <div id="nav" onClick={this.handleClick}>第{this.props.weekCount}周</div>
+      </>
     )
   }
 }
